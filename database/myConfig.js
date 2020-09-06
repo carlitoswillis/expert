33d4c1dae3a24061719faff83b06bed8420201b0
carlitoswillis <carlitoswillis@berkeley.edu>
2020-09-05 22:06:04 -0700

    db

--- database/myConfig.js
+++ database/myConfig.js
@@ -3,11 +3,31 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: '',
-  database: 'my_db',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB || 'my_db',
+  port: process.env.DB_PORT || 3306,
 });
 
-connection.connect();
+connection.connect((err) => {
+  if (err) console.log(err);
+  console.log('connected');
+});
+
+// connection.query(`CREATE TABLE if not exists sources(
+//   id int AUTO_INCREMENT,
+//   authors text,
+//   title text,
+//   content longtext,
+//   created text,
+//   published text,
+//   url text,
+//   fileID text,
+//   driveLink text,
+//   fileName text,
+//   primary key (id)
+// )`, (err) => {
+//   if (err) console.log(err);
+// });
 
 module.exports = connection;
 
